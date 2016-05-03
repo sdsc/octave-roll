ca184af48f44a7a4fb7fdb226f6ff9c84ffc743c
Jim Hayes <jhayes@sdsc.edu>
2016-05-03 11:41:59 -0700

    Get compiler name to show correctly in whatis info.

--- src/octave-modules/version.mk
+++ src/octave-modules/version.mk
@@ -1,8 +1,13 @@
+ifndef ROLLCOMPILER
+  ROLLCOMPILER = gnu
+endif
+COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
+
 PACKAGE     = octave
 CATEGORY    = applications
 
 NAME        = sdsc-$(PACKAGE)-modules
-RELEASE     = 4
+RELEASE     = 5
 PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
