3f2dcb2228662c374ac7deec8f8cc1d57c50a4a3
Jerry Greenberg <jpg@comet-01-66.local>
2017-03-30 19:01:31 -0700

    APS-435 add several octave packages

--- src/octave-modules/version.mk
+++ src/octave-modules/version.mk
@@ -7,7 +7,7 @@ PACKAGE     = octave
 CATEGORY    = applications
 
 NAME        = sdsc-$(PACKAGE)-modules
-RELEASE     = 5
+RELEASE     = 6
 PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
