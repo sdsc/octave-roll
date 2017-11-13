618a804d043f4cedc14e73fa14fe64fb09bc204b
Jim Hayes <jhayes@sdsc.edu>
2017-11-13 13:16:20 -0800

    Correct path to MKL libraries. Addresses APS-608.

--- src/octave/version.mk
+++ src/octave/version.mk
@@ -10,7 +10,7 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-octave
 VERSION        = 4.2.1
-RELEASE        = 2
+RELEASE        = 3
 PKGROOT        = /opt/octave
 
 SRC_SUBDIR     = octave
