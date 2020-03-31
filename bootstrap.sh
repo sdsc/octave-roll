0bf32ae382ad46c5bdbe905b7c252e548d61bd8b
Jerry Greenberg <jpg@sdsc.edu>
2020-03-31 16:51:06 -0700

    APS-1102 add yum qt packages for octave gui

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+# Install additional system packages required for roll build.
+yum install qt5-qtbase-devel
+yum install qt5-qttools-libs-help
+yum install qt5-doctools
+yum install qt5-qttools.devel
+
